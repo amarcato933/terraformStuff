a028aaf22000066267f04a5d09e574e2318827b9
amarcato933 <54266192+amarcato933@users.noreply.github.com>
2022-07-04 13:06:51 +0200

    Added route and route table

--- main.tf
+++ main.tf
@@ -29,3 +29,20 @@ resource "aws_internet_gateway" "mtc_internet_gateway" {
     Name = "svil-igw"
   }
 }
+
+
+//route table
+resource "aws_route_table" "mtc_public_rt" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "dev_public_rt"
+  }
+}
+
+//route
+resource "aws_route" "default_route" {
+  route_table_id = aws_route_table.mtc_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.mtc_internet_gateway.id
+}

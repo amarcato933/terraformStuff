26a51dd95ce9524e0c52c59990bff57f2b8aa113
amarcato933 <54266192+amarcato933@users.noreply.github.com>
2022-07-05 16:43:34 +0200

    First deploy EC2 instance

--- main.tf
+++ main.tf
@@ -78,3 +78,21 @@ resource "aws_key_pair" "mtc_auth" {
   key_name   = "mtckey"
   public_key = file("~/.ssh/mtckey.pub")
 }
+
+//deploy instance
+resource "aws_instance" "svil_node" {
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.server_ami.id //get ami ID
+
+  tags = {
+    Name = "svil_node"
+  }
+
+  key_name               = aws_key_pair.mtc_auth.id
+  vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+  subnet_id              = aws_subnet.mtc_public_subnet.id
+
+  root_block_device {
+    volume_size = 10 //8 by default
+  }
+}
